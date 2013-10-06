5c7ceaad56d8f255ab992c874a0040ce93eaa38d
Christophe Chapuis <chris.chapuis@gmail.com>
2013-10-06 16:32:45 +0200

    Tests: add settings for Gnex, ready to use for testing higher DPI and resolutions
    Signed-off-by: Christophe Chapuis <chris.chapuis@gmail.com>

--- qml/Tests/imports/LunaNext/SettingsStub.js
+++ qml/Tests/imports/LunaNext/SettingsStub.js
@@ -1,8 +1,14 @@
 .pragma library
-
+/* A500 alike */
 var displayWidth = 1024;
 var displayHeight = 768;
 var dpi = 148;
+/**/
+/*GNex alike
+var displayWidth = 768;
+var displayHeight = 1280;
+var dpi = 311;
+/**/
 var displayFps = true;
 var fontStatusBar = "Prelude"
 var showReticle = false;
