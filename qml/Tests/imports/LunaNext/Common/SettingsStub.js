206639a3e249d62e219b0996332f7112290519dd
Christophe Chapuis <chris.chapuis@gmail.com>
2014-04-22 12:04:18 +0200

    Tests: add a display settings for little laptop
    Signed-off-by: Christophe Chapuis <chris.chapuis@gmail.com>

--- qml/Tests/imports/LunaNext/Common/SettingsStub.js
+++ qml/Tests/imports/LunaNext/Common/SettingsStub.js
@@ -24,7 +24,7 @@ var displayWidth = 1024;
 var displayHeight = 768;
 var dpi = 148;
 /**/
-/*GNex alike*/
+/*GNex alike
 var displayWidth = 768;
 var displayHeight = 1280;
 var dpi = 311;
@@ -33,6 +33,11 @@ var dpi = 311;
 var displayWidth = 1280;
 var displayHeight = 800;
 var dpi = 216;
+/**/
+/* Tofe little laptop */
+var displayWidth = 800;
+var displayHeight = 600;
+var dpi = 114;
 /**/
 var displayFps = true;
 var fontStatusBar = "Prelude"
